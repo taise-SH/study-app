ac9942373e36f6a18b49a8caf1b462000d86cda9
taise-SH <69847025+taise-SH@users.noreply.github.com>
2020-10-19 23:16:58 +0900

    自作問題の表示

--- app/controllers/my_problems_controller.rb
+++ app/controllers/my_problems_controller.rb
@@ -1,14 +1,24 @@
 class MyProblemsController < ApplicationController
 
   def index
-    @my_problem = My_problem.all
+    @problems = MyProblem.all
   end
 
   def new
+    @my_problem = MyProblem.new
+  end
+
+  def show
   end
 
   def create
-    @my_problem = My_problem.new(problem_params)
+    @my_problem = MyProblem.create(problem_params)
+    if @my_problem.valid?
+      @my_problem.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private

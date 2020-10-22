8d1e61be8fac7e63dba0e1fbdb68285dbb7a3ad9
taise-SH <69847025+taise-SH@users.noreply.github.com>
2020-10-22 22:21:42 +0900

    user管理機能のルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "subjects#index"
   resources :subjects
-  resources :englishes
   resources :my_problems
+  resources :users, only: :show
 end

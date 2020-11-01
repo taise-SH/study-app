d94395fc0e9209c25ca218db7f2a3d3f5b9f1951
taise-SH <69847025+taise-SH@users.noreply.github.com>
2020-11-01 09:22:42 +0900

    ユーザーのテストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
   has_many :my_problems
 
   validates :nickname, presence: true
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX }
 end
